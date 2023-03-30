e16bc6629738d49f2f44e951c2123111d613bca1
Navdeep <navdeep.dahiya@sundaymobility.com>
2023-03-30 18:45:26 +0530

    updates

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const liveDb = "mongodb://navdeep:monu1676@127.0.0.1:27017/dc";
+const liveDb = "mongodb://monu:monu1676@127.0.0.1:27017/dc";
 const localDb = "mongodb://0.0.0.0:27017/dc";
 const connectDb = async () => {
   try {
-    await mongoose.connect(localDb);
+    await mongoose.connect(liveDb);
     console.log("conneected db successfully");
   } catch (error) {
     console.log(error.message);
